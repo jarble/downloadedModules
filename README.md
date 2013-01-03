19a21dcb3ecbf5d5c59521976030fdbc84170a9a
anderson <anderson@anderson-Ideapad-Z560.(none)>
2013-01-03 11:50:58 -0500

    Commit message

---
+++ README.md
@@ -0,0 +1,1 @@
+To put the updates on Git, double-click makeGitStuff.sh and enter your username and password.
--- downloadedModules/colorNames.js
+++ downloadedModules/colorNames.js
@@ -2,4 +2,3 @@ function getColorNames(){
 	return(["red", [255, 0, 0], "green", [0, 255, 0], "blue", [0, 0, 255], "black", [0, 0, 0]]);
 }
 exports.getColorNames = getColorNames;
-//Hi!
